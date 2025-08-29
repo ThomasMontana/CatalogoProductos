5762bcfcf63692bed54cec21cf3e69bd7a84ef72
Thomas Montana <91162981+ThomasMontana@users.noreply.github.com>
2025-08-29 11:52:21 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,14 +6,56 @@ fetch("./js/productos.json")
     .then(data => {
         productos = data;
         cargarProd(productos);
-    })
+    });
 
+
+
+let indiceActual = 0;   // desde qué producto empezar
+const cantidadPorPagina = 8;  // cuántos productos mostrar por click
 
 
 /* contenedores de producto */
 const contenedorProductos = document.querySelector("#contenedor-productos");
+const btnCargarMas = document.querySelector("#btn-cargar-mas");
 
-function cargarProd(productosElegidos){
+
+function cargarProd(){
+
+    //contenedorProductos.innerHTML="";
+    // mostrar desde indiceActual hasta indiceActual + cantidadPorPagina
+    
+    const productosAMostrar = productos.slice(indiceActual, indiceActual + cantidadPorPagina);
+
+    productosAMostrar.forEach(producto =>{
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img src="${producto.imagen}" alt="${producto.titulo}">
+            <p class="nombre">${producto.titulo}</p>
+        `;
+        contenedorProductos.append(div);
+        
+    })
+
+    // actualizar índice
+    indiceActual += cantidadPorPagina;
+
+    // ocultar botón si ya no hay más productos
+    if (indiceActual >= productos.length) {
+        btnCargarMas.style.display = "none";
+    }
+
+}
+
+/* evento del botón */
+btnCargarMas.addEventListener("click", () => {
+    cargarProd();
+});
+
+
+// cargar por categoria
+
+function cargarProdCat(productosElegidos){
 
     contenedorProductos.innerHTML="";
 
@@ -26,9 +68,8 @@ function cargarProd(productosElegidos){
         `;
         contenedorProductos.append(div);
     })
+
 }
-
-
 
 
 /* categorias */
@@ -40,9 +81,13 @@ botonesCategorias.forEach(boton =>{
 
         if(e.currentTarget.id != "todos"){
             const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-            cargarProd(productosBoton);
+            cargarProdCat(productosBoton);
+            
+            
         }else{
-            cargarProd(productos);
+            contenedorProductos.innerHTML="";
+            indiceActual = 0;   // desde qué producto empezar
+            cargarProd();
         }
         
     })
